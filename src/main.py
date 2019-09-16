7c540adabd467a9fee7c42572de4844701021a67
kesavarao.tamada <tamada.kesava52@gmail.com>
2019-09-16 05:30:09 +0530

    Added logout

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
 # todo : write main class
-<<<<<<< HEAD
-
 # developed sap integration
 # login fix applied
-=======
 # ensure you have comments
-
-for infosys
->>>>>>> infy_trail
+# working on login
